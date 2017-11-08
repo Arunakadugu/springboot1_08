8bc0c8bc63d07537d017d7fdca0d443fb4438b71
Aruna kadugu <arunakumari.7k@gmail.com>
2017-11-08 11:52:53 -0500

    Initial commit

---
+++ src/main/java/me/aruna/springboot1_08/Springboot108Application.java
@@ -0,0 +1,12 @@
+package me.aruna.springboot1_08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot108Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot108Application.class, args);
+	}
+}
---
+++ src/test/java/me/aruna/springboot1_08/Springboot108ApplicationTests.java
@@ -0,0 +1,16 @@
+package me.aruna.springboot1_08;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot108ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
